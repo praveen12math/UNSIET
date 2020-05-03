07b649f1fa81ec9a74e23ccdf13b0630f89298b5
praveen12math <praveen12math@github.com>
2020-05-03 20:29:35 +0530

    first commit

---
+++ js/loader.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".sidenav").sidenav();
+    $('.datepicker').datepicker();
+    $('.modal').modal();
+    $('.dropdown-trigger').dropdown();
+    $('select').formSelect();
+  }); 
+
+  function toggleModal(){
+    var instance = M.Modal.getInstance($('#modal1'));
+    instance.open();
+
+    var instance = M.Dropdown.getInstance($('#dropdown1'));
+    instance.open();
+
+    var instance = M.FormSelect.getInstance($('#select1'));
+    instance.getSelectedValues();
+  }
